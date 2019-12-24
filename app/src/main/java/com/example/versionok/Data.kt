67ef28be5c1f0af9e1a530d79f66b6261b62de93
AhmMZQ <ahmd.marzaq@gmail.com>
2019-12-24 17:11:50 +0100

    tinder component

---
+++ app/src/main/java/com/example/versionok/Data.kt
@@ -0,0 +1,5 @@
+package com.example.tindercomponent
+
+
+
+class Data(val imagePath: String, val description: String)
---
+++ app/src/main/java/com/example/versionok/tinderComponent.kt
@@ -0,0 +1,12 @@
+package com.example.versionok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tinderComponent : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tinder_component)
+    }
+}
