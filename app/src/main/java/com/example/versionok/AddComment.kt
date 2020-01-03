7402dd8dd2e52dda0393c5d0a3e47b8f65910a1b
AhmMZQ <ahmd.marzaq@gmail.com>
2020-01-03 23:50:41 +0100

    03-01

---
+++ app/src/main/java/com/example/versionok/AddComment.kt
@@ -0,0 +1,14 @@
+package com.example.versionok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddComment : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_comment)
+
+
+    }
+}
